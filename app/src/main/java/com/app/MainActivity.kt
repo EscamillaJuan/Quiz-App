e22efb67ec5fa27a59d35968098d7cfa0e7a0b2a
Salvadorppp <salvadorperezchi@gmail.com>
2024-02-27 08:49:35 -0600

    feat: business logic "Opciones"
    Layout acivity 2 fixed

--- app/src/main/java/com/app/MainActivity.kt
+++ app/src/main/java/com/app/MainActivity.kt
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.app.view.GameScreen
+import com.app.view.OptionsScreen
 
 class MainActivity : AppCompatActivity() {
     private lateinit var openBtn : Button
+    private lateinit var optionBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         openBtn = findViewById(R.id.play_btn)
+        optionBtn = findViewById(R.id.option_btn)
 
         openBtn.setOnClickListener {
             val intent = Intent(this, GameScreen::class.java)
             startActivity(intent)
+        }
+        optionBtn.setOnClickListener {
+            val intentionBtn = Intent(this, OptionsScreen::class.java)
+            startActivity(intentionBtn)
         }
     }
 }
