2bd0c834716364272cae813447936b646719a3ef
Salvadorppp <salvadorperezchi@gmail.com>
2024-03-01 22:12:11 -0600

    Squashed commit of the following:
    commit d643dd93529d01cc69a4a47365b99ccc2df3d901
    Author: EscamillaJuan <juanpedroescamilla@outlook.com>
    Date:   Fri Mar 1 17:44:16 2024 -0600
    
        feat: add logic to answer questions
    
    commit bcd5c27c3537c97d21710308132fc787474a4505
    Author: pedrogxmz <a16001585@alumnos.uady.mx>
    Date:   Fri Mar 1 17:15:31 2024 -0600
    
        fix: Pedro´s bullshit work
    
    commit 7847b62a9876c6255a3b5a42e634cd4393581e5e
    Author: pedrogxmz <a16001585@alumnos.uady.mx>
    Date:   Fri Mar 1 16:50:14 2024 -0600
    
        feat: adding dificult selector
    
    commit 6dcdf491355bd9e6aeb4e1d61b09af87d325ba0c
    Merge: c5da535 4672e7b
    Author: Salvadorppp <84723060+Salvadorppp@users.noreply.github.com>
    Date:   Wed Feb 28 19:03:49 2024 -0600
    
        Merge pull request #17 from EscamillaJuan/ToDo-#01
    
        style: icon app and background color
    
    commit 4672e7b694e76d6391fdf8c83148f0216fe12204
    Author: Salvadorppp <salvadorperezchi@gmail.com>
    Date:   Wed Feb 28 19:02:35 2024 -0600
    
        style: icon app and background color

--- app/src/main/java/com/app/view/GameScreen.kt
+++ app/src/main/java/com/app/view/GameScreen.kt
@@ -1,6 +1,5 @@
 package com.app.view
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -11,10 +10,10 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.app.R
 import com.app.model.GameModel
-import com.google.android.material.snackbar.Snackbar
+const val SELECTED_DIFFICULT = "SELECTED_DIFFICULT"
 
 class GameScreen : AppCompatActivity() {
-    private val mode = "medium"
+
     private val gameModel: GameModel by viewModels()
     private lateinit var nextBtn: ImageButton
     private lateinit var prevBtn: ImageButton
@@ -23,52 +22,15 @@ class GameScreen : AppCompatActivity() {
     private lateinit var questionsCounter: TextView
     private lateinit var topicText: TextView
     private lateinit var topicIcon: ImageView
-    //private var hints = 5
+    private val options = mutableListOf<Button>()
+    private var mode = "medium"
+
     private fun setOptions(mode: String) {
-        val answers = gameModel.getOptions(mode)
         val options = mutableListOf<TextView>()
-
-
         options.add(findViewById(R.id.option_1))
         options.add(findViewById(R.id.option_2))
         options.add(findViewById(R.id.option_3))
         options.add(findViewById(R.id.option_4))
-        when (mode) {
-            "easy" -> {
-
-                for (i in 0 until 2) {
-                    options[i].visibility = View.VISIBLE
-                    options[i].visibility = View.VISIBLE
-                    options[i].text = answers[i].keys.first()
-                }
-                for (i in 2 until 4) {
-                    options[i].visibility = View.GONE
-                    options[i].visibility = View.GONE
-                }
-
-
-            }
-
-            "medium" -> {
-
-                for (i in 0 until 3) {
-                    options[i].visibility = View.VISIBLE
-                    options[i].visibility = View.VISIBLE
-                    options[i].text = answers[i].keys.first()
-                }
-                options[3].visibility = View.GONE
-                options[3].visibility = View.GONE
-            }
-
-            else -> {
-
-                for (i in 0 until 4) {
-                    options[i].visibility = View.VISIBLE
-                    options[i].visibility = View.VISIBLE
-                    options[i].text = answers[i].keys.first()
-                }
-            }
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -82,37 +44,41 @@ class GameScreen : AppCompatActivity() {
         questionsCounter = findViewById(R.id.question_number)
         topicText = findViewById(R.id.topic)
         topicIcon = findViewById(R.id.topic_icon)
+        options.add(findViewById(R.id.option_1))
+        options.add(findViewById(R.id.option_2))
+        options.add(findViewById(R.id.option_3))
+        options.add(findViewById(R.id.option_4))
 
 
-        hintBtn.text = gameModel.currentHintText
         questionText.text = gameModel.currentQuestionText
         questionsCounter.text = gameModel.counterText
         topicText.text = gameModel.topicText
         topicIcon.setImageResource(gameModel.topicIcon)
-        setOptions(mode)
+        mode = intent.getStringExtra(SELECTED_DIFFICULT).toString()
+        gameModel.getOptions(mode, options)
+
 
         nextBtn.setOnClickListener {
-            gameModel.nextQuestion()
+            gameModel.nextQuestion(mode, options)
             questionText.text = gameModel.currentQuestionText
             questionsCounter.text = gameModel.counterText
             topicText.text = gameModel.topicText
             topicIcon.setImageResource(gameModel.topicIcon)
-            setOptions(mode)
         }
 
         prevBtn.setOnClickListener {
-            gameModel.prevQuestion()
+            gameModel.prevQuestion(mode, options)
             questionText.text = gameModel.currentQuestionText
             questionsCounter.text = gameModel.counterText
             topicText.text = gameModel.topicText
             topicIcon.setImageResource(gameModel.topicIcon)
-            setOptions(mode)
         }
-        hintBtn.setOnClickListener { _ ->
-            gameModel.currentHint()
-            hintBtn.text = gameModel.currentHintText
-            //hintBtn.setBackgroundColor(Color.rgb(153, 204, 0))//verde
-            //setBackgroundColor(Color.rgb(0, 153, 204))//rojo
+
+        for( i in 0 until options.size - 1) {
+            options[i].setOnClickListener {
+                gameModel.checkAnswer(options[i].text, this)
+            }
         }
+
     }
 }
