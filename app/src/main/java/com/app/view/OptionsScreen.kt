068bffababb332287d2f6254d300e6ca16f09a15
pedrogxmz <a16001585@alumnos.uady.mx>
2024-04-23 19:16:38 -0600

    Añadido de xml options screen

--- app/src/main/java/com/app/view/OptionsScreen.kt
+++ app/src/main/java/com/app/view/OptionsScreen.kt
@@ -3,9 +3,12 @@ package com.app.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -15,24 +18,35 @@ import com.app.model.GameModel
 
 class OptionsScreen : AppCompatActivity()  {
         private lateinit var opText: TextView
+        private lateinit var themeText: TextView
+        private lateinit var checkbx: List<CheckBox>
         private lateinit var prevBtn: Button
-        private lateinit var workText: TextView
-        private lateinit var bldIcon: ImageView
+        private lateinit var modeSp: Spinner
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.options_screen)
-
+            modeSp = findViewById(R.id.difficulty_spin)
             opText = findViewById(R.id.optionsText)
-            prevBtn = findViewById(R.id.return_options_btn)
-            workText = findViewById(R.id.workinprogressText)
-            bldIcon = findViewById(R.id.workinprogress_icon)
+            themeText = findViewById(R.id.TemasText)
+
+
+
+            ArrayAdapter.createFromResource(
+                this,
+                R.array.modes_array,
+                android.R.layout.simple_spinner_item
+            ).also {
+                it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                modeSp.adapter = it
+            }
+
+
 
 
             prevBtn.setOnClickListener {
                 finish()
             }
-
 
         }
 
